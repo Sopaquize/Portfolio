a37a4573f61f40b0cbcefd906991ace38c65d2a3
Sopaquize <166601290+Sopaquize@users.noreply.github.com>
2025-01-20 15:56:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-// Changer de thème
+// Fonction pour changer de thème
 document.getElementById('themeToggle').addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
+    const themeIcon = document.getElementById('themeIcon');
+    if (document.body.classList.contains('dark-theme')) {
+        themeIcon.innerHTML = '&#9789;';  // Icône de soleil
+    } else {
+        themeIcon.innerHTML = '&#9733;';  // Icône de lune
+    }
 });
 
 // Afficher la description des centres d'intérêt
